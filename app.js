248829c381f0ff5afd060f1b65cc40cce3d0db17
Alex Ainea <xo.ainea@gmail.com>
2024-04-02 04:59:52 +0300

    end

---
+++ app.js
@@ -0,0 +1,10 @@
+// COPY
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+// COPY END
